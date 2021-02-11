0134df32a572d53030383e879f7ace530b8f5f69
hyperdefined <13475823+hyperdefined@users.noreply.github.com>
2021-02-11 18:53:51 -0500

    forgot to add copyright

--- simplehub-bukkit/src/main/java/lol/hyper/simplehub/ReloadCommand.java
+++ simplehub-bukkit/src/main/java/lol/hyper/simplehub/ReloadCommand.java
@@ -1,7 +1,23 @@
+/*
+ * This file is part of SimpleHub.
+ *
+ * SimpleHub is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimpleHub is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimpleHub.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package lol.hyper.simplehub;
 
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
